65fc11a000a1576b01cc1984b1899fb942b112ad
Vlad Mikin <asura93@yandex.ru>
2022-02-28 21:57:02 +0300

    Исправление замечаний №1

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,3 +1,5 @@
+import io.qameta.allure.Step;
+
 public class Account {
     private final String name;
 
@@ -6,38 +8,70 @@ public class Account {
     }
 
     public boolean checkNameToEmboss() {
-        /*
-             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
-             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
-         */
-        String[] split = name.split(" ");
+        if (nameIsNull(name)) {
+            return false;
+        } else if (nameIsEmpty(name)) {
+            return false;
+        } else if (nameIsToLong(name)) {
+            return false;
+        } else if (nameIsToShort(name)) {
+            return false;
+        } else if (nameIsBlank(name)) {
+            return false;
+        } else if (nameIsStartWithBlank(name)) {
+            return false;
+        } else if (nameIsEndWithBlank(name)) {
+            return false;
+        } else if (nameIsWithoutSurname(name)) {
+            return false;
+        } else return !nameIsWithPatronymic(name);
+    }
 
-        if (name.isEmpty()) {
-            System.out.println("Имя отсутствует.");
-            return false;
-        } else if (name.length() > 19) {
-            System.out.println("Имя слишком длинное");
-            return false;
-        } else if (name.length() < 3) {
-            System.out.println("Имя слишком короткое.");
-            return false;
-        } else if (name.isBlank()) {
-            System.out.println("Имя не должно состоять из пробелов");
-            return false;
-        } else if (name.startsWith(" ")) {
-            System.out.println("Имя не должно содержать пробелов в начале");
-            return false;
-        } else if (name.endsWith(" ")) {
-            System.out.println("Имя не должно содержать пробелов в конце");
-            return false;
-        } else if (split.length<2) {
-            System.out.println("Имя должно состоять из имени и фамилии");
-            return false;
-        } else if (split.length>2) {
-            System.out.println("Имя должно состоять из имени и фамилии");
-            return false;
-        }
-        System.out.println("Подходящее имя");
-        return true;
+    @Step("Имя отсутствует")
+    public boolean nameIsEmpty(String name) {
+        return name.isEmpty();
+    }
+
+    @Step("Имя слишком длинное")
+    public boolean nameIsToLong(String name) {
+        return name.length() > 19;
+    }
+
+    @Step("Имя слишком короткое")
+    public boolean nameIsToShort(String name) {
+        return name.length() < 3;
+    }
+
+    @Step("Имя не должно состоять из пробелов")
+    public boolean nameIsBlank(String name) {
+        return name.isBlank();
+    }
+
+    @Step("Имя не должно содержать пробелов в начале")
+    public boolean nameIsStartWithBlank(String name) {
+        return name.startsWith(" ");
+    }
+
+    @Step("Имя не должно содержать пробелов в конце")
+    public boolean nameIsEndWithBlank(String name) {
+        return name.endsWith(" ");
+    }
+
+    @Step("Не указана фамилия")
+    public boolean nameIsWithoutSurname(String name) {
+        String[] split = name.split(" ");
+        return split.length < 2;
+    }
+
+    @Step("Отчество не нужно указывать")
+    public boolean nameIsWithPatronymic(String name) {
+        String[] split = name.split(" ");
+        return split.length > 2;
+    }
+
+    @Step("Имя Null")
+    public boolean nameIsNull(String name) {
+        return name == null;
     }
 }
+
--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,44 +1,46 @@
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import io.qameta.allure.junit4.DisplayName;
 
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class AccountTest {
+    static String str = null;
     private final String name;
     private final Boolean expected;
+    private final String message;
 
 
-    public AccountTest(String name, Boolean expected) {
+    public AccountTest(String name, Boolean expected, String message) {
         this.name = name;
         this.expected = expected;
+        this.message = message;
     }
 
     @Parameterized.Parameters
     public static Object[][] getNameData() {
-        return new Object[][] {
-                {" Петр Великий", false},
-                {"Петр Великий ", false},
-                {"ПетрВеликий", false},
-                {" Петр Первый Великий ", false},
-                {"Петр Первый Великий", false},
-                {"ПВ", false},
-                {"Петр", false},
-                {"ПетрПервый  Великий", false},
-                {"ПетрПервый ОченьОченьВеликий", false},
-                {"", false},
-                {"Петр Первый", true},
+        return new Object[][]{
+                {" Петр Великий", false, "Проверка - пробел в начале строки"},
+                {"Петр Великий ", false, "Проверка - пробела в конце строки"},
+                {"ПетрВеликий", false, "Проверка - наличия пробела между именем и фамилие"},
+                {str, false, "Проверка - Null"},
+                {"Петр Первый Великий", false, "Проверка - вводено отчество"},
+                {"ПВ", false, "Проверка - в строке меньше 3 символов"},
+                {"П В", true, "Проверка - граничное значения в 3 символа"},
+                {"ПетрПервый  Великий", false, "Проверка - пробел должен быть только 1"},
+                {"ПетрПервый ОченьОченьВеликий", false, "Проверка - в строке больше 19 символов"},
+                {"", false, "Проверка - имя не должно быть пустым"},
+                {"Петр Первый", true, "Проверка - имя корректно"},
+                {"Девятнадцать Символ", true, "Проверка - граничное значение в 19 символов"},
         };
     }
 
     @Test
-    @DisplayName("Параметризированный тест")
+    @DisplayName("Параметризованный тест")
     public void nameWillBeValidTest() {
         Account account = new Account(name);
-        assertEquals(expected, account.checkNameToEmboss());
+        assertEquals("", expected, account.checkNameToEmboss());
     }
-
 }
